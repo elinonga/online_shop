5f4646732f95e4b31adb230c8b5c1373cc28e6d4
mezza <44901625+elinonga@users.noreply.github.com>
2022-12-21 10:37:45 +0300

    :fire: Tailwind CSS

--- layouts/products.vue
+++ layouts/products.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
 
-        <header>
-            <nav>
-                <NuxtLink to="/products">Kwanza App Products</NuxtLink>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4">
+                <NuxtLink to="/products" class="font-bold">Kwanza App Products</NuxtLink>
             </nav>
         </header>
 
         <!-- Output page content -->
-        <div>
+        <div class="container mx-auto p-4">
             <slot />
         </div>
 
-        <footer>
-            <ul>
+        <footer class="container mx-auto p-4 flex justify-between border-t-2">
+            <ul class="flex gap-4">
                 <li><NuxtLink to="/">Home</NuxtLink></li>
                 <li><NuxtLink to="/about">About</NuxtLink></li>
                 <li><NuxtLink to="/products">Products</NuxtLink></li>
